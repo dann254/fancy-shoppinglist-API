00b04361d953084872597d5a11bff5a63ce03715
Daniel Kipkemei <daniel.kipkemei@andela.com>
2017-10-14 15:56:51 +0300

    [CHORE [#151906232]]reconfigure tests

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,7 +1,7 @@
 import unittest
 import json
 from app import create_app, db
-from app.email_handler import generate_token, decode_token
+from app.email_handler import generate_token
 
 class AuthTest(unittest.TestCase):
     """Test authentication: login and register"""
@@ -30,7 +30,7 @@ class AuthTest(unittest.TestCase):
             db.drop_all()
             db.create_all()
             self.confirm_token=generate_token('email@mail.com')
-            self.d=decode_token(self.confirm_token)
+
     def test_registration(self):
         """Test if user registration works correctly and user is saved to db"""
         reqst = self.client().post(self.register_route, data=self.user_data)
@@ -48,8 +48,9 @@ class AuthTest(unittest.TestCase):
         """Test if the registered user can login"""
         reqst = self.client().post(self.register_route, data=self.user_data)
         self.assertEqual(reqst.status_code, 201)
-        self.client().get(self.confirm_route + '{}'.format(self.confirm_token))
-        self.assertEqual(self.d, 'email@mail.com')
+        vr = self.client().get(self.confirm_route + '{}'.format(self.confirm_token))
+        rst = json.loads(vr.data.decode())
+        self.assertEqual(vr['message'], 'email@mail.com')
         login_reqst = self.client().post(self.login_route, data=self.user_data)
         #get jsonified result and test if it  returns 200 status
         result = json.loads(login_reqst.data.decode())
