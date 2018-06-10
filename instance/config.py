1e65f9b44023d96df1ea775e270dd49309b0e3c6
Daniel Kipkemei <daniel.kipkemei@andela.com>
2018-06-10 22:26:39 +0300

    update config

--- instance/config.py
+++ instance/config.py
@@ -6,7 +6,7 @@ class Config(object):
     CSRF_ENABLED = True
     SECRET = os.getenv('SECRET')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    MAIL_SERVER = 'nbo2.domainskenya.co.ke'
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
     MAIL_PORT = '465'
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
